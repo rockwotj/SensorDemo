9d0f64efe25fff6710c12aa9dbb0160dc9017da0
Tyler Rockwood <rockwotj@gmail.com>
2019-01-17 13:51:31 -0800

    inclass 2

--- app/src/main/java/edu/rosehulman/sensordemo/pickshake/ShakeDetector.java
+++ app/src/main/java/edu/rosehulman/sensordemo/pickshake/ShakeDetector.java
@@ -2,11 +2,19 @@ package edu.rosehulman.sensordemo.pickshake;
 
 
 import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+import edu.rosehulman.sensordemo.MathUtils;
 
 /** A detector that waits for the device to be shook along the device's Y axis. */
 public class ShakeDetector {
 
-    private Listener mListener;
+    private final SensorManager mManager;
+    private final Sensor mSensor;
+    private ShakeListener mListener;
 
     /** A listener for shake events. */
     public interface Listener {
@@ -15,15 +23,43 @@ public class ShakeDetector {
     }
 
     public ShakeDetector(Context context) {
-        // TODO: Implement me!
+        mManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+    }
+
+    private static class ShakeListener implements SensorEventListener {
+
+        private static final float SHAKE_THRESHOLD = 4;
+        private Listener listener;
+        private float previousY;
+
+        public ShakeListener(Listener listener) {
+            this.listener = listener;
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            float y = sensorEvent.values[1];
+            if (!MathUtils.withinDelta(y, previousY, SHAKE_THRESHOLD)) {
+                listener.onShake();
+            }
+            previousY = y;
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+            // ignore
+        }
     }
 
     public void registerListener(Listener listener) {
-        // TODO: Implement me!
+        mListener = new ShakeListener(listener);
+        mManager.registerListener(mListener, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     public void unregisterListener() {
-        // TODO: Implement me!
+        mManager.unregisterListener(mListener);
+        mListener = null;
     }
 
 }
--- app/src/main/java/edu/rosehulman/sensordemo/scrolltilt/ScrollTiltActivity.java
+++ app/src/main/java/edu/rosehulman/sensordemo/scrolltilt/ScrollTiltActivity.java
@@ -9,6 +9,7 @@ import edu.rosehulman.sensordemo.R;
 public class ScrollTiltActivity extends Activity {
 
     private NestedScrollView mScrollView;
+    private TiltDetector mDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +18,34 @@ public class ScrollTiltActivity extends Activity {
         getActionBar().setDisplayHomeAsUpEnabled(true);
         mScrollView = findViewById(R.id.scroll_view);
         // mScrollView.smoothScrollBy(dx, dy);
+        mDetector = new TiltDetector(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mDetector.registerListener(new TiltDetector.Listener() {
+            static final double SCROLL_MULTIPLIER = 20;
+
+            @Override
+            public void onTilt(TiltDetector.Direction direction, double magnitude) {
+                int dy = 0;
+                switch (direction) {
+                    case TOP:
+                        dy = (int) (magnitude * SCROLL_MULTIPLIER);
+                        break;
+                    case BOTTOM:
+                        dy = (int) (magnitude * -SCROLL_MULTIPLIER);
+                        break;
+                }
+                mScrollView.smoothScrollBy(0, dy);
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mDetector.unregisterListener();
     }
 }
