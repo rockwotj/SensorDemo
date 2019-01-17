22957bfcea8067038a6635ba8b36cc1f2e494bdd
Tyler Rockwood <rockwotj@gmail.com>
2019-01-17 08:17:53 -0800

    Added scroll demo

--- app/src/main/java/edu/rosehulman/sensordemo/scrolltilt/ScrollTiltActivity.java
+++ app/src/main/java/edu/rosehulman/sensordemo/scrolltilt/ScrollTiltActivity.java
@@ -8,7 +8,10 @@ import edu.rosehulman.sensordemo.R;
 
 public class ScrollTiltActivity extends Activity {
 
+    private final static int SCROLL_MULTIPLIER = 20;
+
     private NestedScrollView mScrollView;
+    private TiltDetector mTiltDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +19,32 @@ public class ScrollTiltActivity extends Activity {
         setContentView(R.layout.activity_scroll_tilt);
         getActionBar().setDisplayHomeAsUpEnabled(true);
         mScrollView = findViewById(R.id.scroll_view);
-        // mScrollView.smoothScrollBy(dx, dy);
+        mTiltDetector = new TiltDetector(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mTiltDetector.registerListener(new TiltDetector.Listener() {
+            @Override
+            public void onTilt(TiltDetector.Direction direction, double magnitude) {
+                int dy = 0;
+                switch (direction) {
+                    case TOP:
+                        dy = (int) (magnitude * SCROLL_MULTIPLIER);
+                        break;
+                    case BOTTOM:
+                        dy = (int) (magnitude * -SCROLL_MULTIPLIER);
+                        break;
+                }
+                mScrollView.smoothScrollBy(0, dy);
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mTiltDetector.unregisterListener();
     }
 }
